5f009184e613dc45cc5ff3a21e9226d249c8681f
ThanhDat653 <datnt.653@gmail.com>
2023-03-24 12:29:03 +0700

    responsive for landing page

---
+++ client/assets/javaScript/landing/responsive.js
@@ -0,0 +1,16 @@
+
+const responsiveFunction = function() {
+    const footerElement = document.getElementById('footer');
+    const containerHeight = document.getElementById('container').offsetHeight;
+    const viewHeight = window.innerHeight;
+
+    if(viewHeight < 1150) 
+        footerElement.style.top = `${containerHeight + 100} px`
+    else 
+        footerElement.style.bottom = '0';
+};
+
+window.onresize = responsiveFunction;
+window.onload = responsiveFunction;
+
+// Test API
