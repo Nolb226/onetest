ac7a63b3bce967e6f38b737d7bb30bdd36376a47
ThanhDat653 <datnt.653@gmail.com>
2023-03-31 12:38:38 +0700

    validator for sign up

--- test/src/components/modal/signUp.jsx
+++ test/src/components/modal/signUp.jsx
@@ -3,309 +3,304 @@ import { useState, useEffect } from "react";
 import validator from "./validator.js";
 
 function Options() {
-  return <option>Công nghệ thông tin</option>;
+   return (
+      <select id="majors" name="majors" type="select" className="form-control">
+         <option>Ngành</option>
+         <option>Công nghệ thông tin</option>
+      </select>
+   );
 }
 
 function StudentSignUp() {
-  validator("#form-register");
+   validator("#form-register");
 
-  return (
-    <div id="student-sign-up">
-      <div className="form-group">
-        <input
-          rules="require"
-          id="fullname"
-          name="fullname"
-          type="text"
-          placeholder="Nhập họ và tên sinh viên"
-          className="form-control"
-        />
-        <span className="form-message"></span>
+   return (
+      <div id="student-sign-up">
+         <div className="form-group">
+            <input
+               rules="require"
+               id="fullname"
+               name="fullname"
+               type="text"
+               placeholder="Nhập họ và tên sinh viên"
+               className="form-control"
+            />
+            <label htmlFor="fullname" className="form-message"></label>
+         </div>
+
+         <div
+            className="row flex-center flex-direction-row"
+            style={{ width: "100%", height: "auto", margin: "0" }}
+         >
+            <div className="l-5 m-5 form-group position-relative">
+               <input
+                  rules="require"
+                  id="dob"
+                  name="dob"
+                  type="date"
+                  className="form-control"
+               />
+               <span className="form-message"></span>
+            </div>
+
+            <div className="l-7 m-7 form-group position-relative">
+               <Options />
+               <span className="form-message"></span>
+            </div>
+         </div>
+
+         <div className="form-group">
+            <input
+               rules="require|min:10|max:10"
+               id="personalID"
+               name="username"
+               type="text"
+               placeholder="Nhập mã sinh viên"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
+
+         <div className="form-group position-relative">
+            <input
+               rules="require|min:8"
+               id="password"
+               name="password"
+               type="password"
+               placeholder="Nhập mật khẩu"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
+
+         <div className="form-group position-relative">
+            <input
+               rules="require"
+               id="password-confirm"
+               name="password-confirm"
+               type="password"
+               placeholder="Nhập lại mật khẩu"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
       </div>
-
-      <div
-        className="row flex-center flex-direction-row"
-        style={{ width: "100%", height: "auto", margin: "0" }}
-      >
-        <div className="l-5 m-5 form-group position-relative">
-          <input
-            rules="require"
-            id="date-of-birth"
-            name="date-of-birth"
-            type="date"
-            className="form-control"
-          />
-          <span className="form-message"></span>
-        </div>
-
-        <div className="l-7 m-7 form-group position-relative">
-          <select
-            id="majors"
-            name="majors"
-            type="select"
-            className="form-control"
-          >
-            <Options />
-          </select>
-          <span className="form-message"></span>
-        </div>
-      </div>
-
-      <div className="form-group">
-        <input
-          rules="require|min:10|max:10"
-          id="personalID"
-          name="personalID"
-          type="text"
-          placeholder="Nhập mã sinh viên"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-
-      <div className="form-group position-relative">
-        <input
-          rules="require|min:8"
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Nhập mật khẩu"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-
-      <div className="form-group position-relative">
-        <input
-          rules="require"
-          id="password-confirm"
-          name="password-confirm"
-          type="password"
-          placeholder="Nhập lại mật khẩu"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-    </div>
-  );
+   );
 }
 
 function TeacherSignUp() {
-  validator("#form-register");
+   validator("#form-register");
 
-  return (
-    <div id="teacher-sign-up">
-      <div className="form-group">
-        <input
-          rules="require"
-          id="fullname"
-          name="fullname"
-          type="text"
-          placeholder="Nhập họ và tên giảng viên"
-          className="form-control"
-        />
-        <span className="form-message"></span>
+   return (
+      <div id="teacher-sign-up">
+         <div className="form-group">
+            <input
+               rules="require"
+               id="fullname"
+               name="fullname"
+               type="text"
+               placeholder="Nhập họ và tên giảng viên"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
+
+         <div
+            className="row flex-center flex-direction-row"
+            style={{ width: "100%", height: "auto", margin: "0" }}
+         >
+            <div className="l-5 m-5 form-group position-relative">
+               <input
+                  rules="dateOfBirth"
+                  id="dob"
+                  name="dob"
+                  type="date"
+                  className="form-control"
+               />
+               <span className="form-message"></span>
+            </div>
+
+            <div className="l-7 m-7 form-group position-relative">
+               <Options />
+               <span className="form-message"></span>
+            </div>
+         </div>
+
+         <div className="form-group">
+            <input
+               rules="require|min:10|max:10"
+               id="personalID"
+               name="username"
+               type="text"
+               placeholder="Nhập mã cán bộ"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
+
+         <div className="form-group position-relative">
+            <input
+               rules="require|min:8"
+               id="password"
+               name="password"
+               type="password"
+               placeholder="Nhập mật khẩu"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
+
+         <div className="form-group position-relative">
+            <input
+               rules="require"
+               id="password-confirm"
+               name="password-confirm"
+               type="password"
+               placeholder="Nhập lại mật khẩu"
+               className="form-control"
+            />
+            <span className="form-message"></span>
+         </div>
       </div>
-
-      <div
-        className="row flex-center flex-direction-row"
-        style={{ width: "100%", height: "auto", margin: "0" }}
-      >
-        <div className="l-5 m-5 form-group position-relative">
-          <input
-            rules="require"
-            id="date-of-birth"
-            name="date-of-birth"
-            type="date"
-            className="form-control"
-          />
-          <span className="form-message"></span>
-        </div>
-
-        <div className="l-7 m-7 form-group position-relative">
-          <select
-            id="departments"
-            name="departments"
-            type="select"
-            className="form-control"
-          >
-            <Options />
-          </select>
-          <span className="form-message"></span>
-        </div>
-      </div>
-
-      <div className="form-group">
-        <input
-          rules="require|min:10|max:10"
-          id="personalID"
-          name="personalID"
-          type="text"
-          placeholder="Nhập mã cán bộ"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-
-      <div className="form-group position-relative">
-        <input
-          rules="require|min:8"
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Nhập mật khẩu"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-
-      <div className="form-group position-relative">
-        <input
-          rules="require"
-          id="password-confirm"
-          name="password-confirm"
-          type="password"
-          placeholder="Nhập lại mật khẩu"
-          className="form-control"
-        />
-        <span className="form-message"></span>
-      </div>
-    </div>
-  );
+   );
 }
 
 function TypeAccount({ setType }) {
-  return (
-    <div className="typeAccount flex-center position-absolute">
-      <div
-        className="student flex-center flex-direction-col"
-        onClick={() => {
-          setType.handleSetType("student");
-        }}
-      >
-        <i className="fa-solid fa-user-graduate"></i>
-        <span>Sinh viên</span>
-      </div>
+   return (
+      <div className="typeAccount flex-center position-absolute">
+         <div
+            className="student flex-center flex-direction-col"
+            onClick={() => {
+               setType.handleSetType("student");
+            }}
+         >
+            <i className="fa-solid fa-user-graduate"></i>
+            <span>Sinh viên</span>
+         </div>
 
-      <div
-        className="teacher flex-center flex-direction-col"
-        onClick={() => {
-          setType.handleSetType("teacher");
-        }}
-      >
-        <i className="fa-solid fa-user-tie"></i>
-        <span>Giảng viên</span>
+         <div
+            className="teacher flex-center flex-direction-col"
+            onClick={() => {
+               setType.handleSetType("teacher");
+            }}
+         >
+            <i className="fa-solid fa-user-tie"></i>
+            <span>Giảng viên</span>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 function SignUpModal({ toggle1, toggle2 }) {
-  const [type, setType] = useState("true");
+   const [type, setType] = useState("true");
 
-  function handleSetType(string) {
-    setType(string);
-  }
+   function handleSetType(string) {
+      setType(string);
+   }
 
-  useEffect(() => {
-    validator("#form-register");
-  }, [type]);
-  return (
-    <div
-      className="modal-layer flex-center"
-      onClick={() => {
-        toggle2.handleSignUp();
-      }}
-    >
+   useEffect(() => {
+      validator("#form-register");
+   }, [type]);
+   return (
       <div
-        className="sign-up--modal"
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
+         className="modal-layer flex-center"
+         onClick={() => {
+            toggle2.handleSignUp();
+         }}
       >
-        <form action="" method="POST" id="form-register">
-          <div className="modal__header position-relative">
-            <div className="modal__content flex-center flex-direction-col">
-              <h3 className="modal--heading">Đăng ký</h3>
-              <span className="modal--subheading">
-                Nhập đầy đủ thông tin và tạo tài khoản miễn phí!
-              </span>
-            </div>
-            <div className="tool-box flex-center position-absolute">
-              <div
-                className="modal__back--btn"
-                onClick={() => {
-                  setType("true");
-                }}
-              >
-                <i className="fa-solid fa-angle-left"></i>
-              </div>
-              <div
-                className="modal__close--btn"
-                onClick={() => {
-                  toggle2.handleSignUp();
-                }}
-              >
-                <i className="fa-solid fa-xmark"></i>
-              </div>
-            </div>
-          </div>
+         <div
+            className="sign-up--modal"
+            onClick={(e) => {
+               e.stopPropagation();
+            }}
+         >
+            <form action="" method="POST" id="form-register">
+               <div className="modal__header position-relative">
+                  <div className="modal__content flex-center flex-direction-col">
+                     <h3 className="modal--heading">Đăng ký</h3>
+                     <span className="modal--subheading">
+                        Nhập đầy đủ thông tin và tạo tài khoản miễn phí!
+                     </span>
+                  </div>
+                  <div className="tool-box flex-center position-absolute">
+                     <div
+                        className="modal__back--btn"
+                        onClick={() => {
+                           setType("true");
+                        }}
+                     >
+                        <i className="fa-solid fa-angle-left"></i>
+                     </div>
+                     <div
+                        className="modal__close--btn"
+                        onClick={() => {
+                           toggle2.handleSignUp();
+                        }}
+                     >
+                        <i className="fa-solid fa-xmark"></i>
+                     </div>
+                  </div>
+               </div>
 
-          <div
-            id="modal__body"
-            className="modal__body flex-center flex-direction-col position-relative"
-          >
-            {type == "true" && <TypeAccount setType={{ handleSetType }} />}
-            {type == "student" && <StudentSignUp />}
+               <div
+                  id="modal__body"
+                  className="modal__body flex-center flex-direction-col position-relative"
+               >
+                  {type == "true" && (
+                     <TypeAccount setType={{ handleSetType }} />
+                  )}
+                  {type == "student" && <StudentSignUp />}
 
-            {type == "teacher" && <TeacherSignUp />}
+                  {type == "teacher" && <TeacherSignUp />}
+               </div>
+               <div className="modal__footer flex-center flex-direction-col">
+                  <div className="flex-center form-group accept">
+                     <input id="accept-input" name="accept" type="radio" />
+                     <span>
+                        Tôi đồng ý với
+                        <a href="#">&nbsp;điều khoản sử dụng</a>
+                        &nbsp;và<a href="#">&nbsp;chính sách bảo mật</a>
+                        &nbsp;của Best Of Test
+                     </span>
+                     {document
+                        .querySelectorAll(".accept > span  a")
+                        .forEach((item) => {
+                           item.onClick = (event) => {
+                              console.log(item);
+                              alert(
+                                 "Chính sách bảo mật và điều khoản sử dụng đang được cập nhật!"
+                              );
+                              event.preventDefault();
+                           };
+                        })}
+                  </div>
 
-            <div className="flex-center form-group accept">
-              <input id="accept-input" name="accept" type="radio" />
-              <span>
-                Tôi đồng ý với<a href="#">&nbsp;điều khoản sử dụng</a>
-                &nbsp;và<a href="#">&nbsp;chính sách bảo mật</a>
-                &nbsp;của Best Of Test
-              </span>
-              {document
-                .querySelectorAll(".accept > span  a")
-                .forEach((item) => {
-                  item.onClick = (event) => {
-                    console.log(item);
-                    alert(
-                      "Chính sách bảo mật và điều khoản sử dụng đang được cập nhật!"
-                    );
-                    event.preventDefault();
-                  };
-                })}
-            </div>
+                  <button
+                     className="form-submit"
+                     onClick={() => {
+                        // toggle2.handleSignUp();
+                     }}
+                  >
+                     Đăng ký
+                  </button>
 
-            <button
-              className="form-submit"
-              onClick={() => {
-                toggle2.handleSignUp();
-              }}
-            >
-              Đăng ký
-            </button>
-
-            <div className="have-account">
-              <span>Bạn đã có tài khoản? </span>
-              <span
-                className="click-to-sign-in"
-                onClick={() => {
-                  toggle2.handleSignUp();
-                  toggle1.handleSignIn();
-                }}
-              >
-                Đăng nhập ngay
-              </span>
-            </div>
-          </div>
-        </form>
+                  <div className="have-account">
+                     <span>Bạn đã có tài khoản? </span>
+                     <span
+                        className="click-to-sign-in"
+                        onClick={() => {
+                           toggle2.handleSignUp();
+                           toggle1.handleSignIn();
+                        }}
+                     >
+                        Đăng nhập ngay
+                     </span>
+                  </div>
+               </div>
+            </form>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default SignUpModal;
--- test/src/components/modal/validator.js
+++ test/src/components/modal/validator.js
@@ -1,50 +1,124 @@
 function validator(formSelector) {
-  let formElement = document.querySelector(formSelector);
-  let formRules = {};
+   let formElement = document.querySelector(formSelector);
+   let formRules = {};
 
-  var validatorRules = {
-    require: (value) => {
-      return value ? undefined : "Vui lòng nhập thông tin";
-    },
+   const getParentElement = (childElement, parentSelector) => {
+      while (childElement.parentElement) {
+         if (childElement.parentElement.matches(parentSelector)) {
+            return childElement.parentElement;
+         }
+         childElement = childElement.parentElement;
+      }
+   };
 
-    dateOfBirth: (value) => {
-      return value ? undefined : "Vui lòng nhập thông tin";
-    },
+   var validatorRules = {
+      require: (value) => {
+         return value ? undefined : "Vui lòng nhập thông tin";
+      },
 
-    min: (min) => {
-      return (value) =>
-        value.length < min ? undefined : `Tối thiểu ${min} kí tự`;
-    },
+      dateOfBirth: (value) => {
+         return value ? undefined : "Vui lòng chọn ngày tháng năm sinh";
+      },
 
-    max: (max) => {
-      return (value) =>
-        value.length >= max ? undefined : `Tối đa ${max} kí tự`;
-    },
-  };
+      major: (value) => {
+         return value !== "Ngành" ? undefined : "Vui lòng chọn ngành học";
+      },
 
-  if (formElement) {
-    var inputs = formElement.querySelectorAll("[name][rules]");
+      min: (min) => {
+         return (value) =>
+            value.length >= min ? undefined : `Tối thiểu ${min} kí tự`;
+      },
 
-    inputs.forEach((input) => {
-      var rules = input.getAttribute("rules").split("|");
+      max: (max) => {
+         return (value) =>
+            value.length <= max ? undefined : `Tối đa ${max} kí tự`;
+      },
+   };
 
-      rules.forEach((rule) => {
-        if (rule.includes(":")) {
-          var temp = rule.split(":");
-          rule = temp[0];
-        }
+   if (formElement) {
+      var inputs = formElement.querySelectorAll("[name][rules]");
 
-        console.log(rule);
+      const clearErrorMessage = (event) => {
+         let parentElement = getParentElement(event.target, ".form-group");
+         parentElement.classList.remove("invalid");
+         parentElement.querySelector(".form-message").innerText = "";
+      };
+
+      // Lắng nghe sự kiện trên từng thẻ input
+      const handelValidate = (event) => {
+         console.log(event);
+         var rules = formRules[event.target.name];
+         var errorMessage;
+
+         rules.find(function (rule) {
+            errorMessage = rule(event.target.value);
+            return errorMessage;
+         });
+
+         let parentElement = getParentElement(event.target, ".form-group");
+
+         if (errorMessage) {
+            parentElement.classList.add("invalid");
+            parentElement.querySelector(
+               ".form-message"
+            ).innerText = `* ${errorMessage}`;
+         }
+
+         return !errorMessage;
+      };
+
+      // Lặp và gán function validator cho từng thẻ input
+      inputs.forEach((input) => {
+         var rules = input.getAttribute("rules").split("|");
+
+         rules.forEach((rule) => {
+            var ruleFunction;
+            ruleFunction = validatorRules[rule];
+
+            if (rule.includes(":")) {
+               // rule = "min:8"
+               var temp = rule.split(":"); // temp[0] = "min", temp[1] = "8"
+               rule = temp[0]; // rule = "min"
+               ruleFunction = validatorRules[rule]; // ruleFunction = min()
+               ruleFunction = ruleFunction(temp[1]); // ruleFunction =  min(8) return (value) =>  value.length > min ? undefined : `Tối thiểu ${8} kí tự`;
+            }
+
+            if (Array.isArray(formRules[input.name]))
+               formRules[input.name].push(ruleFunction);
+            else formRules[input.name] = [ruleFunction];
+         });
+
+         input.onblur = handelValidate;
+         input.oninput = clearErrorMessage;
       });
 
-      formRules[input.name] = input.getAttribute("rules");
-    });
+      formElement.onsubmit = (event) => {
+         event.preventDefault();
+         var isValid = true;
+         let formData = new FormData();
 
-    // formRules.forEach((formRule) => {
-    //   console.log(typeof formRule);
-    // });
-    console.log(formRules);
-  }
+         inputs.forEach((input) => {
+            if (!handelValidate({ target: input })) {
+               isValid = false;
+            }
+         });
+
+         if (isValid) {
+            console.log("submit");
+            // formElement.submit();
+
+            inputs.forEach((input) => {
+               formData.append(input.name, input.value);
+            });
+         }
+         fetch("http://localhost:8080/auth/signup", {
+            body: formData,
+            method: "post",
+         });
+      };
+   }
 }
 
 export default validator;
+
+class Validate {}
