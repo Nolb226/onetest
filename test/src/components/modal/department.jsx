e75ccab7b97087f9e421b897dfe82b786113e79c
ThanhDat653 <datnt.653@gmail.com>
2023-04-01 14:15:58 +0700

    s

--- test/src/components/modal/department.jsx
+++ test/src/components/modal/department.jsx
@@ -1,6 +1,21 @@
 import "../../css/grid.css";
+async function setDepartments() {
+   try {
+      const resonse = await fetch("http://localhost:8080/departments/", {});
+
+      if (!resonse.ok) throw new Error("Failed to get department from server!");
+
+      const departments = await resonse.json();
+
+      console.log(departments);
+   } catch (error) {
+      console.log(error);
+   }
+}
 
 function Options() {
+   setDepartments();
+
    return (
       <div className="form-group position-relative" style={{ width: "auto" }}>
          <label htmlFor="majors" className="form-label">
