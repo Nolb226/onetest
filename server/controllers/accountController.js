4cbc5264e428ff4185f5aa754dde6681ad2881eb
Nolb226 <hn26677@gmail.com>
2023-04-15 19:27:25 +0700

    update backend

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -2,6 +2,7 @@ const { successResponse, errorResponse } = require('../util/helper');
 
 exports.getUserAccount = async (req, res, _) => {
 	try {
+		successResponse(res, 200, req.user);
 	} catch (error) {
 		errorResponse(res, error);
 	}
