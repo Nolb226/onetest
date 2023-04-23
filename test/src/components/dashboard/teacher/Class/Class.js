1ddf74cd4318e79c62b4bdd0b6e80adea72bbb98
ThanhDat653 <datnt.653@gmail.com>
2023-04-23 12:45:48 +0700

    delete gridClass of Son

--- test/src/components/dashboard/teacher/Class/Class.js
+++ test/src/components/dashboard/teacher/Class/Class.js
@@ -1,40 +1,39 @@
-import Classes from './Classes';
-import { useState } from 'react';
-import Repass from './Repass';
-import ClassList from './ClassList';
-import CreateClass from './CreateClass';
-import './gridClass.css';
-import './styleClass.css';
-import { Outlet } from 'react-router';
+import Classes from "./Classes";
+import { useState } from "react";
+import Repass from "./Repass";
+import ClassList from "./ClassList";
+import CreateClass from "./CreateClass";
+import "./styleClass.css";
+import { Outlet } from "react-router";
 
 function Class() {
-	const [classesPage, setClassesPage] = useState(true);
-	const [repassPage, setRepassPage] = useState(false);
-	const [classListPage, setClassListPage] = useState(false);
-	const [createClassPage, setCreateClassPage] = useState(false);
-	const [isClass, setIsClass] = useState({});
+   const [classesPage, setClassesPage] = useState(true);
+   const [repassPage, setRepassPage] = useState(false);
+   const [classListPage, setClassListPage] = useState(false);
+   const [createClassPage, setCreateClassPage] = useState(false);
+   const [isClass, setIsClass] = useState({});
 
-	const handleRePass = (Class) => {
-		setRepassPage(!repassPage);
-		setClassesPage(!classesPage);
-		setIsClass(Class);
-		console.log(1);
-	};
+   const handleRePass = (Class) => {
+      setRepassPage(!repassPage);
+      setClassesPage(!classesPage);
+      setIsClass(Class);
+      console.log(1);
+   };
 
-	const handleClassList = (Class) => {
-		setClassesPage(!classesPage);
-		setClassListPage(!classListPage);
-		setIsClass(Class);
-	};
+   const handleClassList = (Class) => {
+      setClassesPage(!classesPage);
+      setClassListPage(!classListPage);
+      setIsClass(Class);
+   };
 
-	const handleCreateClass = () => {
-		setClassesPage(!classesPage);
-		setCreateClassPage(!createClassPage);
-	};
+   const handleCreateClass = () => {
+      setClassesPage(!classesPage);
+      setCreateClassPage(!createClassPage);
+   };
 
-	return (
-		<>
-			{/* {repassPage && <Repass isClass={isClass} handleRePass = {handleRePass} />}
+   return (
+      <>
+         {/* {repassPage && <Repass isClass={isClass} handleRePass = {handleRePass} />}
          {classesPage && (
             <Classes
                handleRePass={handleRePass}
@@ -44,9 +43,9 @@ function Class() {
          )}
          {classListPage && <ClassList isClass={isClass} handleClassList = {handleClassList} />}
          {createClassPage && <CreateClass handleCreateClass = {handleCreateClass} />} */}
-			<Outlet />
-		</>
-	);
+         <Outlet />
+      </>
+   );
 }
 
 export default Class;
--- test/src/config/config.js
+++ test/src/config/config.js
@@ -1,6 +1,6 @@
 // const api = "http://192.168.100.30:8080"; moda
-// const api = "http://192.168.1.91:8080"; blue lagoon
-const api = "http://192.168.89.236:8080";
+const api = "http://192.168.1.164:8080"; //blue lagoon
+// const api = "http://192.168.89.236:8080";
 // lupis
 
 // const api = "http://localhost:8080";
