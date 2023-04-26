b6d2e4fa1ab2fb44dc4b0dceabd9b69e90a8c10d
Nolb226 <hn26677@gmail.com>
2023-04-26 08:58:59 +0700

    frontend

--- test/src/config/config.js
+++ test/src/config/config.js
@@ -3,6 +3,7 @@
 // const api = "http://192.168.89.236:8080";
 // lupis
 
-const api = 'http://192.168.89.236:8080';
+// const api = 'http://192.168.89.236:8080';
+const api = 'http://localhost:8080';
 
 export default api;
