0ee04af6153ddd9b006a05dbea3fe1dcfcb10bfe
ngocson741852963 <117715756+ngocson741852963@users.noreply.github.com>
2023-05-03 15:27:44 +0700

    a

--- test/src/components/dashboard/teacher/Class/CreateClass.js
+++ test/src/components/dashboard/teacher/Class/CreateClass.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import excel from "../../../../image/excel.svg";
 import api from "../../../../config/config";
+import { useNavigate } from "react-router";
 
 function CreateClass({ handleCreateClass }) {
    const [file, setFile] = useState({
@@ -60,7 +61,7 @@ function CreateClass({ handleCreateClass }) {
             Authorization: "Bearer " + currentUser,
          },
       });
-      handleCreateClass();
+      navigator(-1)
    };
 
    // quay lại
