f3ce565513787b85f2b0402889ddc86e2bc691bd
ThanhDat653 <datnt.653@gmail.com>
2023-05-12 00:09:46 +0700

    fix z-index, change icon delete permission group, css locked view result of exam

--- test/src/components/dashboard/student/component-student/component-studentpage/ExamItem.js
+++ test/src/components/dashboard/student/component-student/component-studentpage/ExamItem.js
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function ExamButton({ classId, exam, isDone, isLock }) {
-	/* 
+   /* 
 		Button_Status :
 		- "Done" but not "Lock"
 		- "Lock" but not "Done"
@@ -12,100 +12,100 @@ function ExamButton({ classId, exam, isDone, isLock }) {
 		3. isDone & !isLock => Enable button
 	*/
 
-	let status;
-	if (isDone && !isLock) {
-		status = 'Done';
+   let status;
+   if (isDone && !isLock) {
+      status = "Done";
 
-		return (
-			<>
-				<Link
-					to={`../../../exam/${exam}`}
-					state={{ classId, isDone: true }}
-					relative="path"
-				>
-					<button class="exam-done view-btn">Xem</button>
-				</Link>
-			</>
-		);
-	}
-	if (isDone && isLock) {
-		status = 'Lock';
+      return (
+         <>
+            <Link
+               to={`../../../exam/${exam}`}
+               state={{ classId, isDone: true }}
+               relative="path"
+            >
+               <button class="exam-done view-btn">Xem</button>
+            </Link>
+         </>
+      );
+   }
+   if (isDone && isLock) {
+      status = "Lock";
 
-		return <button class="inf-btn exam-lock">Đã khóa</button>;
-	}
-	return (
-		<>
-			<Link
-				to={`../../../exam/${exam}`}
-				state={{ classId, isDone: false }}
-				relative="path"
-			>
-				<button class="view-btn" style={{ backgroundColor: '#161f89' }}>
-					Làm
-				</button>
-			</Link>
-		</>
-	);
+      return <button class="inf-btn exam-lock">Đã khóa</button>;
+   }
+   return (
+      <>
+         <Link
+            to={`../../../exam/${exam}`}
+            state={{ classId, isDone: false }}
+            relative="path"
+         >
+            <button class="view-btn" style={{ backgroundColor: "#161f89" }}>
+               Làm
+            </button>
+         </Link>
+      </>
+   );
 }
 
 function ExamItem({
-	idExam,
-	nameExam,
-	subject,
-	time,
-	timeStart,
-	timeEnd,
-	totalQuestions,
-	isDone,
-	classId,
-	isLock,
-	exam,
+   idExam,
+   nameExam,
+   subject,
+   time,
+   timeStart,
+   timeEnd,
+   totalQuestions,
+   isDone,
+   classId,
+   isLock,
+   exam,
 }) {
-	return (
-		<ul
-			className="flex-center table__content--item"
-			style={{
-				display: 'grid',
-				gridTemplateColumns: '8% 14% 18% 20% 20% 10% 5% 5%',
-			}}
-		>
-			<li className="flex-center column-text">
-				<h3>{idExam}</h3>
-			</li>
+   return (
+      <ul
+         className="flex-center table__content--item"
+         style={{
+            display: "grid",
+            gridTemplateColumns: "8% 14% 18% 20% 20% 10% 10%",
+         }}
+      >
+         <li className="flex-center column-text">
+            <h3>{idExam}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				<h3>{nameExam}</h3>
-			</li>
+         <li className="flex-center column-text">
+            <h3>{nameExam}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				<h3>{subject}</h3>
-			</li>
+         <li className="flex-center column-text">
+            <h3>{subject}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				{/* <h3>{vietNamFomatter.format(new Date(timeStart))}</h3> */}
-				<h3>{timeStart}</h3>
-			</li>
+         <li className="flex-center column-text">
+            {/* <h3>{vietNamFomatter.format(new Date(timeStart))}</h3> */}
+            <h3>{timeStart}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				{/* <h3>{vietNamFomatter.format(new Date(timeEnd))}</h3> */}
-				<h3>{timeEnd}</h3>
-			</li>
+         <li className="flex-center column-text">
+            {/* <h3>{vietNamFomatter.format(new Date(timeEnd))}</h3> */}
+            <h3>{timeEnd}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				<h3>{time}</h3>
-			</li>
+         <li className="flex-center column-text">
+            <h3>{time}</h3>
+         </li>
 
-			<li className="flex-center column-text">
-				<ExamButton
-					key={exam.id}
-					classId={classId}
-					exam={exam}
-					isDone={isDone}
-					isLock={isLock}
-				/>
-			</li>
-		</ul>
-	);
+         <li className="flex-center column-text">
+            <ExamButton
+               key={exam.id}
+               classId={classId}
+               exam={exam}
+               isDone={isDone}
+               isLock={isLock}
+            />
+         </li>
+      </ul>
+   );
 }
 
 export default ExamItem;
